6c68f29115177ea34d7569c66abd863095496d0d
CarlosRivera4726 <carlosrivera_-717@hotmail.com>
2023-04-23 09:51:32 -0500

    Modelo de vendedor

---
+++ pagina_web_corte_2/models/VendedorModel.php
@@ -0,0 +1,39 @@
+<?php 
+    class Vendedor{
+        private $id;
+        private $id_usuario;
+        private $numero_cuenta;
+
+        public function __construct($id_usuario, $numero_cuenta){
+            $this->id_usuario = $id_usuario;
+            $this->numero_cuenta = $numero_cuenta;
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function setId($id){
+            $this->id = $id;
+        }
+        public function getIdUsuario(){
+            return $this->id_usuario;
+        }
+
+        public function setIdUsuario($id_usuario){
+            $this->id_usuario = $id_usuario;
+        }
+        public function getNumeroCuenta(){
+            return $this->numero_cuenta;
+        }
+
+        public function setNumeroCuenta($numero_cuenta){
+            $this->numero_cuenta = $numero_cuenta;
+        }
+        
+
+        public function __toString(){
+            return "ID: ". $this->id." id_usuario: ".$this->id_usuario." numero cuenta: ".$this->numero_cuenta;
+        }
+    }
+?>
